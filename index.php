630428c2f66132a6de9054215acd285f7a3d0623
dyushes <67632839+dyushes@users.noreply.github.com>
2022-03-14 22:33:48 +0500

    start_project

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="./assets/style.css">
+        <title>Andrey's album!</title>
+    </head>
+
+    <header class="header">
+        <div class="container">
+            <div class="header_inner">
+                <div class="header_logo">Мой альбом</div>
+                <nav class="nav">
+
+                    <ul class="top_menu">
+                        <li><a href="#" class="li_menu">Главная</a></li>
+                        <li><a href="#" class="li_menu"> Проекты</a></li>
+
+                        <li>
+                            <a href="#" class="submenu_link">Фотоальбом</a>
+                        </li>
+                    </ul>
+
+                </nav>
+
+            </div>
+        </div>
+    </header>
+
+    <body>
+
+        <div class="intro">
+
+            <div class="container">
+            </div>
+
+        </div>
+
+    </body>
+</html>
